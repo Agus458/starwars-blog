4feae68fc9f5f3c2fcb5ab3ea245abeea7d8bc73
Agus458 <aguspe458@outlook.com>
2021-05-12 20:11:47 +0000

    CORRECION SEARCH

--- src/js/component/SearchBar.js
+++ src/js/component/SearchBar.js
@@ -1,40 +1,26 @@
-import React, { useRef, useContext } from "react";
+import React, { useRef, useContext, useState } from "react";
 import { Context } from "../store/appContext";
 
 export default function SearchBar() {
 	const { store, actions } = useContext(Context);
-	let autoCompleteBox = useRef();
+
+	const [list, setList] = useState([]);
 
 	const autoComplete = event => {
 		let search = event.target.value;
 
-		let result = store.characters.filter(elem => {
-			return elem.name.toLowerCase().startsWith(search.toLowerCase());
-		});
+		if (search.length > 0) {
+			let result = [];
 
-		console.log(result);
+			store.characters.forEach(elem => {
+				if (elem.name.toLowerCase().startsWith(search.toLowerCase())) {
+					result.push({ id: elem.uid, name, type: "peoples" });
+				}
+			});
 
-		if (result.length > 0) {
-			// autoCompleteBox.current.innerHTML = result.map((elem, index) => {
-			// 	return (
-			// 		<li key={index} className="list-group-item list-group-item-action">
-			// 			{elem.name}
-			// 		</li>
-			// 	);
-			// });
-			console.log(
-				result.map((elem, index) => {
-					return (
-						<li key={index} className="list-group-item list-group-item-action">
-							{elem.name}
-						</li>
-					);
-				})
-			);
+			setList(result);
 		} else {
-			autoCompleteBox.current.innerHTML = (
-				<li className="list-group-item list-group-item-action">No results...</li>
-			);
+			setList([]);
 		}
 	};
 
@@ -42,7 +28,15 @@ export default function SearchBar() {
 		<div className="container d-flex justify-content-center">
 			<div className="col-12 col-md-6">
 				<input className="form-control" type="text" placeholder="Type to search.." onKeyUp={autoComplete} />
-				<ul className="list-group autocom-box" ref={autoCompleteBox} />
+				<ul className="list-group autocom-box">
+					{list.map((elem, index) => {
+						return (
+							<li key={index} className="list-group-item list-group-item-action">
+								{elem.name}
+							</li>
+						);
+					})}
+				</ul>
 			</div>
 		</div>
 	);
